9a3f136d3c495159934afa4207f7394f1a716f4c
amjmarshall <113568258+amjmarshall@users.noreply.github.com>
2023-05-24 13:18:33 -0700

    Create insert_data.sh

---
+++ insert_data.sh
@@ -0,0 +1,28 @@
+cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
+
+do
+if [[ $YEAR != "year" ]]
+then
+  #get team id
+  WINNER_ID=$($PSQL "SELECT team_id FROM teams where name='$WINNER'")
+
+  if [[ -z $WINNER_ID ]]
+  then 
+   echo $($PSQL "INSERT INTO teams(name) VALUES ('$WINNER')")
+  fi  
+
+  OPPONENT_ID=$($PSQL "SELECT team_id FROM teams where name='$OPPONENT'")
+  if [[ -z $OPPONENT_ID ]]
+  then 
+   echo $($PSQL "INSERT INTO teams(name) VALUES ('$OPPONENT')")
+  fi  
+
+WINNER_ID=$($PSQL "SELECT team_id FROM teams where name='$WINNER'")
+OPPONENT_ID=$($PSQL "SELECT team_id FROM teams where name='$OPPONENT'")
+
+echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ($YEAR, '$ROUND', $WINNER_ID, $OPPONENT_ID, $WINNER_GOALS, $OPPONENT_GOALS)")
+
+fi
+
+
+done
